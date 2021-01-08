8ede1722176344260bc5c1f24e7659c6cf976334
Kristine <kristinemonsada@gmail.com>
2021-01-08 05:19:55 +0000

    added gifs for kissy and added hug command

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ import os
 import requests
 import json
 from awakeMe import keep_alive
+from discord.ext import commands
 
 client = discord.Client()
 
+def __init__(self):
+  self.bot = discord.Client()
+
+# random compliment generator
 def get_quote():
   response = requests.get("https://complimentr.com/api")
   json_data = json.loads(response.text)
@@ -18,25 +23,30 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+  # make sure msgs are diff from user and bot prevent spammies
   if message.author == client.user:
     return
 
+  # random compliment generator
   if message.content.startswith('!compliment'):
     quote = get_quote()
     await message.channel.send(quote)
 
+  # command to make bot kissy
   if message.content.startswith('!kiss'):
-    msg = '*kisses* {0.author.mention}'.format(message)
-    await message.channel.send(msg)
+    mention = message.author.mention
+    response = f"**kisses** {mention}"
+    await message.channel.send(response)
+    await message.channel.send(file=discord.File('img/bunnykiss.gif'))
+  
+  # command to make bot huggies
+  if message.content.startswith('!hug'):
+    mention = message.author.mention
+    response = f"**hugs** {mention}"
+    await message.channel.send(response)  
+    await message.channel.send(file=discord.File('img/cathug.gif')) 
 
-
-
-#Bot = commands.Bot(command_prefix="")
-
-#@Bot.event
-#  if "kiss" in message.content:
-#    await Bot.send_message("kisses " + {client.user.name})
-
+# function to allow bot to run on repl webserver
 keep_alive()
 
 client.run(os.getenv('TOKEN'))
